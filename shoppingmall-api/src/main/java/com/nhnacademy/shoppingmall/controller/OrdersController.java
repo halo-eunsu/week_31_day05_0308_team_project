5bf2986dc8676c527b40b5ec2f92473f035af062
supermicrosoft <30441597+supermicrosoft@users.noreply.github.com>
2024-03-07 16:47:19 +0900

    Fixed OrdersController

--- shoppingmall-api/src/main/java/com/nhnacademy/shoppingmall/controller/OrdersController.java
+++ shoppingmall-api/src/main/java/com/nhnacademy/shoppingmall/controller/OrdersController.java
@@ -1,6 +1,6 @@
 package com.nhnacademy.shoppingmall.controller;
 
-import com.nhnacademy.shoppingmall.entity.order.OrderRepository;
+import com.nhnacademy.shoppingmall.entity.order.OrderService;
 import com.nhnacademy.shoppingmall.entity.order.Orders;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,26 +18,31 @@ import java.util.Optional;
 @RequestMapping("/api/order")
 public class OrdersController {
 
-    private final OrderRepository orderRepository;
+    private final OrderService orderService;
 
     @PostMapping("/{orderId}")
     public ResponseEntity<?> getOrder(@PathVariable int orderId) {
-        Optional<Orders> ordersOptional = orderRepository.findByOrderId(orderId);
-        if (ordersOptional.isPresent()) {
-            Orders orders = ordersOptional.get();
-
-            HashMap<String, String> resp = new HashMap<>();
-            resp.put("orderDate", String.valueOf(orders.getOrderDate()));
-            resp.put("shipDate", String.valueOf(orders.getShipDate()));
-            resp.put("orderId", String.valueOf(orders.getOrderId()));
-            resp.put("userId", orders.getUser().getId());
-            resp.put("userName", orders.getUser().getName());
-            resp.put("userPoint", String.valueOf(orders.getUser().getPoint()));
+        Optional<Orders> optionalOrders = orderService.getOrders(orderId);
+        if (optionalOrders.isPresent()) {
+            HashMap<String, String> resp = getStringStringHashMap(optionalOrders);
             return ResponseEntity.ok(resp);
         }
         HashMap<String, String> errorResp = new HashMap<>();
         errorResp.put("error", "Invalid orderId");
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResp);
 
+    }
+
+    private static HashMap<String, String> getStringStringHashMap(Optional<Orders> optionalOrders) {
+        Orders orders = optionalOrders.get();
+
+        HashMap<String, String> resp = new HashMap<>();
+        resp.put("orderDate", String.valueOf(orders.getOrderDate()));
+        resp.put("shipDate", String.valueOf(orders.getShipDate()));
+        resp.put("orderId", String.valueOf(orders.getOrderId()));
+        resp.put("userId", orders.getUser().getId());
+        resp.put("userName", orders.getUser().getName());
+        resp.put("userPoint", String.valueOf(orders.getUser().getPoint()));
+        return resp;
     }
 }
--- shoppingmall-api/src/main/java/com/nhnacademy/shoppingmall/entity/order/OrderServiceImpl.java
+++ shoppingmall-api/src/main/java/com/nhnacademy/shoppingmall/entity/order/OrderServiceImpl.java
@@ -2,11 +2,13 @@ package com.nhnacademy.shoppingmall.entity.order;
 
 import lombok.AllArgsConstructor;
 import lombok.Setter;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Setter
 @AllArgsConstructor
+@Service
 public class OrderServiceImpl implements OrderService{
 
     private OrderRepository repository;
